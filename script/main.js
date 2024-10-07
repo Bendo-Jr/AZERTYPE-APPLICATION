0ac8e671c893bf3863d65cdead83e7bbc5f65ace
Bendo-Jr <danio.radaniel@student.passerellesnumeriques.org>
2024-10-07 21:59:58 +0200

    jsfs
    dogdfv

---
+++ script/main.js
@@ -0,0 +1,3 @@
+let motsPropose = ["Application", "Dactylo", "Rapide"];
+let phrasePropose = ["Je suis content", 'Tu es content', 'Il est content']
+
---
+++ script/script.js
@@ -0,0 +1,34 @@
+function afficherResultat(score, nbMotsProposes) {
+    // Récupération de la zone dans laquelle on va écrire le score
+    let spanScore = document.getElementById("spanScore ")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    // On place le texte à l'intérieur du span. 
+    spanScore.innerText = affichageScore
+}
+
+function lancerJeu() {
+    let score = 0
+    let motsPropose = 0;
+
+    let inputEcriture = document.getElementById('inputEcriture');
+inputEcriture.addEventListener('keydown', (event) => {
+   event.target.value
+})
+
+inputEcriture.value = ''
+let btnValidation = document.getElementById('btnValidation');
+btnValidation.addEventListener('click', () => {
+    console.log(inputEcriture.value)
+    inputEcriture.value = ''
+})
+}
+
+lancerJeu();
+
+document.addEventListener('keydown', (event) => {
+if (event.key === 'Enter') {
+    btnValidation.click();
+    console.log('clique')
+}
+})
