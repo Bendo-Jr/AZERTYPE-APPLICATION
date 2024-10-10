497bbded7dc58d01ebf2dd32e468b1d2290374df
Bendo-Jr <danio.radaniel@student.passerellesnumeriques.org>
2024-10-10 22:25:38 +0200

    hjfhdjfhh

---
+++ script/popup.js
@@ -0,0 +1,16 @@
+let btnPartager = document.getElementById('btnPartager');
+
+btnPartager.addEventListener('click', () => {
+    let backroundpopup = document.querySelector('.backround-popup');
+    backroundpopup.style.display = "flex"
+})
+
+let btnQuiter = document.querySelector('.btnQuiter');
+btnQuiter.addEventListener('click', () => {
+    let nom = document.getElementById('nom')
+    let email = document.getElementById('email')
+    let backroundpopup = document.querySelector('.backround-popup');
+    backroundpopup.style.display = "none"
+    nom.value = ''
+    email.value = ''
+})
